9612a7ab4d15013e16be6c21271c60e9c3df6614
Hüseyin Ulaş Arslan <huseyinulasarslann@gmail.com>
2022-07-02 05:36:15 +0300

    updated

--- app.js
+++ app.js
@@ -53,44 +53,47 @@ function closeContentOne() {
  
 
   function turnOnMusic(){
-    myMusic.play();
+    music.play();
     playSvg.classList.add('d-none');
     pauseSvg.style.visibility = "visible";
   }
 
   function turnOffMusic(){
-    myMusic.pause();
+    music.pause();
     playSvg.classList.remove('d-none');
     pauseSvg.style.visibility = "hidden";
     
   }
 
+
+
+
   // countdown
 
   var countDownDate = new Date("july 5, 2022 15:37:25").getTime();
 
-// Update the count down every 1 second
+
 var x = setInterval(function() {
 
-  // Get today's date and time
+  
   var now = new Date().getTime();
     
-  // Find the distance between now and the count down date
+  
   var distance = countDownDate - now;
     
-  // Time calculations for days, hours, minutes and seconds
+  
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
     
-  // If the count down is over, write some text 
+  
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("countdown").innerHTML = "EXPIRED";
   }
 }, 1000);
